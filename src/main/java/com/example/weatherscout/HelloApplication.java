7d3369d04f634a46bed80fc76928ac5fae7fda8b
Donato Dolib <d2thedon@gmail.com>
2025-11-29 23:42:27 +0100

    HelloApplication ohne FXML probieren

--- src/main/java/com/example/weatherscout/HelloApplication.java
+++ src/main/java/com/example/weatherscout/HelloApplication.java
@@ -1,18 +1,32 @@
 package com.example.weatherscout;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import javafx.scene.control.Label;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        VBox root = new VBox();
+        Scene scene = new Scene(root, 800, 800);
+
+        Label cityLabel = new Label("City: Vienna");
+        root.getChildren().add(cityLabel);
+
+        Label temperatureLabel = new Label("Temperature: Celsius");
+        root.getChildren().add(temperatureLabel);
+
+        Label humidityLabel = new Label("Humidity: %");
+        root.getChildren().add(humidityLabel);
+
+        Label windSpeedLabel = new Label("Wind speed: km'/'h");
+        root.getChildren().add(windSpeedLabel);
+
+        stage.setTitle("Weather Scout");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.weatherscout {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.weatherscout to javafx.fxml;
-    exports com.example.weatherscout;
-}
