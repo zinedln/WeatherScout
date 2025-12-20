0b8fc3a56fd3fdfbe4117960b82fcb8d0e2070e8
Donato Dolib <d2thedon@gmail.com>
2025-12-20 10:03:52 +0100

    Add city input and weather labels

--- src/main/java/org/example/weatherscout/HelloController.java
+++ src/main/java/org/example/weatherscout/HelloController.java
@@ -2,13 +2,25 @@ package org.example.weatherscout;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
+    private TextField cityInput;
+
+    @FXML
     private Label welcomeText;
+    @FXML
+    private Label temperature;
+    @FXML
+    private Label humidity;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        String city = cityInput.getText();
+        welcomeText.setText("Checking the weather for: " + city);
+
+        temperature.setText("The temperature is: " + temperature.getText() + "°C");
+        humidity.setText("The humidity is: " + humidity.getText() + "%");
     }
 }
